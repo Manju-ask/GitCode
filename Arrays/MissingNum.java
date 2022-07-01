2bb01c3da6f53298f3b0815b117ad1bd37ff924e
Manju-ask <manjuask24@gmail.com>
2022-07-01 08:52:15 +0530

    2 more code added

---
+++ Arrays/MissingNum.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.stream.Collectors;
+
+/*Given an array nums of n integers where nums[i] is in the range [1, n], 
+return an array of all the integers in the range [1, n] that do not appear in nums.*/
+		
+public class MissingNum {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+    	List<Integer> list = new ArrayList<Integer>();
+    	List<Integer> res= new ArrayList<Integer>();
+    	for(Integer i:nums)
+    		list.add(i);
+    	list = list.stream().distinct().sorted().collect(Collectors.toList());
+    	for(int i=0; i<list.size(); i++) {
+    		if(list.get(i)!=i+1)
+    			res.add(list.get(i));
+    	}
+        return res;
+    }
+	public static void main(String[] args) {
+		int[] arr= {4,3,2,7,8,2,3,1};
+		MissingNum missingNum= new MissingNum();
+		List<Integer> list = missingNum.findDisappearedNumbers(arr);
+		System.out.println(list);
+	}
+}
