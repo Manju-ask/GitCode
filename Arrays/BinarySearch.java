d06c67b7bfddf06f0431765c29f5c6f8cd74c04a
Manju-ask <manjuask24@gmail.com>
2022-07-01 18:06:36 +0530

    search with dac

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,35 @@
+package leetcode;
+/*Given an array of integers nums which is sorted in ascending order, 
+and an integer target, write a function to search target in nums.
+If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.*/
+
+class BinarySearch{
+	public static int search(int[] arr, int low, int high,int key){
+		if(high==low)
+			if(arr[low]==key)
+				return low;
+			
+		if(high>low){
+			int mid= (low+high)/2;
+			if(arr[mid]==key)
+				return mid;
+			else if(key<arr[mid])
+				return search(arr,low,mid,key);
+			return search(arr,mid+1,high,key);
+		}
+		return -1;
+	}
+	public static void main(String[] args){
+		int[] arr= {-1,0,3,5,9,12};
+		int key = -10;
+		int result =search(arr,0,arr.length-1,key);
+		if(result>=0)
+			System.out.println(key+" found! at index "+result);
+		else
+			System.out.println("Key Not found!");
+	}
+}
+
+
--- Arrays/DistinctPairsKdiff.java
+++ Arrays/DistinctPairsKdiff.java
@@ -1,28 +1,31 @@
 package leetcode;
-
 import java.util.Arrays;
+/*Count all distinct pairs with difference equal to k
+Difficulty Level : Medium
+Given an integer array and a positive integer k,
+count all distinct pairs with differences equal to k. 
+
+Examples: 
+
+Input: arr[] = {1, 5, 3, 4, 2}, k = 3
+Output: 2
+There are 2 pairs with difference 3, the pairs are {1, 4} and {5, 2}*/
 
 public class DistinctPairsKdiff {
 	static int distinctPairs(int arr[], int k)
 	{
 		int count = 0;
 		Arrays.sort(arr);   //1 2 3 4 5
-		for(int i=0;i<arr.length; i++) {
-			int j =Arrays.binarySearch(arr, i+1, arr.length-1, arr[i]+k);
-			if(j>0)
+		for(int i=0; i<arr.length-1; i++) {
+			int j =Arrays.binarySearch(arr, i+1, arr.length, arr[i]+k);
+			if(j>=0)
 				count++;
 		}
-		//		for (int i = 0; i < arr.length; i++)
-		//		{
-		//			for (int j = i + 1; j < arr.length; j++)
-		//				if (arr[i] - arr[j] == k || arr[j] - arr[i] == k)
-		//					count++;
-		//		}
 		return count;
 	}
 	public static void main(String[] args) {
-		int arr[] = { 1, 5, 3, 4, 2 };
-		int k = 3;
+		int arr[] = { 8, 12, 16, 4, 0, 20 };   
+		int k = 4;
 		int res= distinctPairs(arr, k);
 		System.out.println(res);
 	}
